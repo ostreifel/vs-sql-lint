7a6d26832b872d223d357a472b5e0e5c87ea1ee4
Otto Streifel <ottost@microsoft.com>
2017-11-24 15:11:24 -0800

    gen classifier

---
+++ sql-lint/SqlClassifierClassificationDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace sql_lint
+{
+    /// <summary>
+    /// Classification type definition export for SqlClassifier
+    /// </summary>
+    internal static class SqlClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "SqlClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("SqlClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
